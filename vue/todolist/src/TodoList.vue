26532d0252721b0d9775ca1bebdb78f73e945fff
xiaojian95 <15616324416@163.com>
2018-04-21 22:34:40 +0800

    tijiao

---
+++ vue/todolist/src/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <div>
+    <input type="text" v-model="inputValue">
+    <button @click="hanleSubmit">提交</button>
+      </div>
+    <ul>
+      <todo-item v-for="(item,index) in list" :key="index"
+      :content="item" :index="index" @delete="hanleDelete">
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    hanleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    hanleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/todolist/src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+<li class="item" @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+<!--有scoped的时候代表这个样式只对你当前的组件有效，如果不加就是全局的一个样式-->
+<style scoped>
+.item{
+  color:green;
+}
+</style>
